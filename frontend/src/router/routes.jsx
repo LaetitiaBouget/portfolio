4ff5e5f080b823602b6b85edba7535b3ee612f10
Laetitia Bouget <laetitia.bouget@yahoo.com>
2025-06-05 10:33:05 +0200

    routing

---
+++ frontend/src/router/routes.jsx
@@ -0,0 +1,34 @@
+import Layout from '../components/Layout';
+import Home from '../pages/Home';
+import Project from '../pages/Project';
+import About from '../pages/About';
+import Contact from '../pages/Contact';
+import NotFound from '../pages/NotFound';
+
+const routes = [
+    {
+        path: "/",
+        Component: Layout,
+        errorElement: <NotFound />,
+        children: [
+            {
+                index: true,
+                Component: Home
+            },
+            {
+                path: 'project',
+                Component: Project
+            },
+            {
+                path: 'about',
+                Component: About
+            },
+            {
+                path: 'contact',
+                Component: Contact
+            },
+        ],
+    },
+];
+
+export default routes;
