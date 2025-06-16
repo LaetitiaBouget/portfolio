5afa2c48cb099c9a55c196515731eb1142027331
Laetitia Bouget <laetitia.bouget@yahoo.com>
2025-06-16 15:08:14 +0200

    style page projet

--- frontend/src/components/ProjectDetails.jsx
+++ frontend/src/components/ProjectDetails.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from 'react-router';
 import { Link } from 'react-router';
 import SlideShow from './SlideShow';
+import ArrowReturn from '../assets/images/arrow-return.JPG'
 
 const ProjectDetails = () => {
   const project = useLoaderData();
@@ -12,40 +13,43 @@ const ProjectDetails = () => {
   ];
 
   return (
-    <section className="project__container">
+    <section className="project-details__container">
 
-      <Link to="/">Retour</Link>
-      <h1>{project.title}</h1>
+      <Link to="/" className="link__return"><img src={ArrowReturn} className="arrow__return"></img> Retour</Link>
+      <h1 className="project__title">{project.title}</h1>
 
-      <div className= "project__infos">
-        <div className= "project__data">
-          <h2>Présentation du projet</h2>
-          <p>{project.description}</p>
-          <p>{project.url}</p>
-          <p>{project.techStack.join(', ')}</p>
-          <p>{new Date(project.deliveryDate)
-            .toLocaleDateString('fr-FR', { year: 'numeric', month:'long',})
-            .toUpperCase()}
-          </p>
+      <div className= "project__data">
+        <div className="project__infos">
+          <h2 className="section__title">Présentation du projet</h2>
+
+            <p>{project.description}</p>
+            <p>{project.url}</p>
+            <p>{project.techStack.join(', ')}</p>
+            <p>{new Date(project.deliveryDate)
+              .toLocaleDateString('fr-FR', { year: 'numeric', month:'long',})
+              .toUpperCase()}
+            </p>
         </div>
-          
+
         <div className="project__missions">
-          <h2>Missions réalisées</h2>
-          <ul>
-            {project.missions.map((mission, index) => (
-              <li key={index}>{mission}</li>
-            ))}
-          </ul>
+            <h2 className="section__title">Missions réalisées</h2>
+
+        
+            <ul>
+              {project.missions.map((mission, index) => (
+                <li key={index}>{mission}</li>
+              ))}
+            </ul>
+
         </div>
-      </div>
-
+        </div>
+      <h2 className="section__title experience">Retour d’expérience</h2>
       <div className="project__experience">
-        <h2>Retour d’expérience</h2>
-
+        
         {experienceSections.map(({ title, key }) => (
           project[key]?.length > 0 && (
             <div className="experience__block" key={key}>
-              <h3>{title}</h3>
+              <h3 className="experience__block__title">{title}</h3>
               <ul>
                 {project[key].map((item, index) => (
                   <li key={index}>{item}</li>
@@ -55,8 +59,11 @@ const ProjectDetails = () => {
           )
         ))}
       </div>
-
-      <SlideShow imageUrls = {project.images} />      
+      
+      <h2 className="section__title gallery">Le projet en images</h2>
+      <div className="project__gallery">
+        <SlideShow imageUrls = {project.images} />      
+      </div>
     </section>
   );
 };
