c3265ee888387cd8f102aa0647928061cd7ed25f
Laetitia Bouget <laetitia.bouget@yahoo.com>
2025-06-16 15:40:29 +0200

    animation vignettes

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -1,7 +1,10 @@
+import codePreview from '../assets/images/3.png'
+
 const ProjectCard = ({project}) => {
     return (
         <div className="project__card">
-            <img src={project.coverImage}></img>
+            <img src={project.coverImage} className="front"></img>
+            <img src={codePreview} className="back"></img>
         </div>
     )
 } 
