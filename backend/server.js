8caad06ecb863fb06e8f21627e2adfa0e4c3a9c5
Laetitia Bouget <laetitia.bouget@yahoo.com>
2025-06-17 20:34:57 +0200

    logs server

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
-require ('dotenv').config();
+require('dotenv').config();
 const mongoose = require('mongoose');
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(() => {
     console.log('Connecté à MongoDB');
     app.listen(PORT, () => {
-        console.log(`Serveur lancé sur http://localhost:${PORT}`);
+        console.log(`Serveur lancé sur le port ${PORT}`);
     });
 })
 .catch((err) => {
-    console.error('Erreur de connexion à MongoDB:', err);
+    console.error('Erreur de connexion à MongoDB:', err.message);
 });
