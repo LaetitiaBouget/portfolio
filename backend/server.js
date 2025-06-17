2bb40919552a27465b8ee7e6bc9607ea3c400581
Laetitia Bouget <laetitia.bouget@yahoo.com>
2025-06-17 19:03:22 +0200

    log

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,3 @@
-const fs = require('fs');
-const util = require('util');
-
-const logFile = fs.createWriteStream('./error.log', { flags: 'a' });
-const logStdout = process.stdout;
-
-console.error = function (msg) {
-  logFile.write(util.format(msg) + '\n');
-  logStdout.write(util.format(msg) + '\n');
-};
-
-
 require ('dotenv').config();
 const mongoose = require('mongoose');
 const app = require('./app');
